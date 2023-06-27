ea7a5c49581c9deb08e88cf24a3cb35d93e693ee
Huang-Xinzhong <huangxinzhong@storswift.com>
2023-06-27 09:43:48 +0800

    fix: 1. Precision Control reconfiguration of leo code generated from flaot data 2. Refactoring of if else logic generation function in Leo code

--- src/k_means/generate_k_means_leo.py
+++ src/k_means/generate_k_means_leo.py
@@ -31,7 +31,7 @@ def generate_struct(centers, str_list_inputs):
     temp2 = []
     str_list_inputs.append("\tstruct Axis{")
     for index in range(0, len(centers[0])):
-        temp1.append("\t\tp" + str(index) + ": i128")
+        temp1.append("\t\tp" + str(index) + ": i32")
         if index == len(centers[0]) - 1:
             temp1.append("\n\t}\n")
         else:
@@ -52,18 +52,26 @@ def decimal_significant_digits(centers):
     output = []
     for row in range(len(centers)):
         temp = []
+        average = 0
         for col in range(len(centers[row])):
-            temp.append(float('%.3g' % centers[row][col]))
+            average += centers[row][col]
+        average = average / len(centers[row])
+        if max(centers[row]) / 2 > average:
+            for col in range(len(centers[row])):
+                temp.append(round(centers[row][col], 2))
+        else:
+            for col in range(len(centers[row])):
+                temp.append(float('%.3g' % centers[row][col]))
         output.append(temp)
     return output
 
 
 def generate_transition(centers, accuracy, str_list_inputs):
-    str_list_inputs.append("\ttransition main(public dataset: Axis) -> i32 {")
+    str_list_inputs.append("\ttransition main(dataset: Axis) -> i32 {")
     for row in range(len(centers)):
         line = ["\t\tlet point" + str(row) + ": Axis = Axis{"]
         for col in range(len(centers[row])):
-            line.append("\n\t\t\tp" + str(col) + ": " + str(int(centers[row][col] * accuracy)) + "i128")
+            line.append("\n\t\t\tp" + str(col) + ": " + str(int(centers[row][col] * accuracy)) + "i32")
             if col != len(centers[row]) - 1:
                 line.append(",")
             else:
@@ -72,15 +80,9 @@ def generate_transition(centers, accuracy, str_list_inputs):
         str_list_inputs.append(''.join(line))
 
     for row in range(0, len(centers)):
+        line = ["\t\tlet e" + str(row) + ": i32 = "]
         for col in range(0, len(centers[row])):
-            str_list_inputs.append(
-                "\t\tlet point_e" + str(row) + "_p" + str(col) + ": i128 = point" + str(row) + ".p" + str(
-                    col) + "-dataset.p" + str(col) + ";")
-
-    for row in range(0, len(centers)):
-        line = ["\t\tlet e" + str(row) + ": i128 = "]
-        for col in range(0, len(centers[row])):
-            line.append("(point_e" + str(row) + "_p" + str(col) + "*" + "point_e" + str(row) + "_p" + str(col) + ")")
+            line.append("(point"+str(row)+".p" + str(col) + "-dataset.p" + str(col) + ")**2u32")
             if col != len(centers[row]) - 1:
                 line.append("+")
             else:
@@ -88,19 +90,40 @@ def generate_transition(centers, accuracy, str_list_inputs):
         str_list_inputs.append(''.join(line))
 
     str_list_inputs.append("\n\t\tlet min_ele_index:i32 = 0i32;")
-    for index in range(len(centers) - 1):
-        str_list_inputs.append("\t\tif (e" + str(index) + " > e" + str(index + 1) + "){")
-        str_list_inputs.append("\t\t\tmin_ele_index = " + str(index + 1) + "i32;")
-        str_list_inputs.append("\t\t}else{")
-        str_list_inputs.append("\t\t\tmin_ele_index = " + str(index) + "i32;")
-        str_list_inputs.append("\t\t}")
+
+    def generata_table_by_numbers(number):
+        return "\t" * number
+
+    def generate_if_else_leo(euclidean_nums, code_list, table_number):
+        compare_times = euclidean_nums - 1
+        if (compare_times-1) < 0:
+            return
+
+        # generate first "if“ code
+        generate_if = generata_table_by_numbers(table_number)+f"if (e{compare_times} > e{compare_times-1}) "+"{"
+        generate_assignment = generata_table_by_numbers(table_number+1)+f"min_ele_index = {compare_times-1}u32;"
+        code_list.append(''.join(generate_if))
+        code_list.append(''.join(generate_assignment))
+        generate_if_else_leo(euclidean_nums-1, code_list, table_number+1)
+
+        # generate first "else“ code
+        generate_else = generata_table_by_numbers(table_number)+"} else {"
+        generate_assignment = generata_table_by_numbers(table_number + 1) + f"min_ele_index = {compare_times - 1}u32;"
+        code_list.append(''.join(generate_else))
+        code_list.append(''.join(generate_assignment))
+        generate_if_else_leo(euclidean_nums-1, code_list, table_number+1)
+        end = generata_table_by_numbers(table_number)+"}"
+        code_list.append(''.join(end))
+
+
+    generate_if_else_leo(len(centers), str_list_inputs, 2)
     str_list_inputs.append("\t\treturn min_ele_index;")
     str_list_inputs.append("\t}")
 
 
 # output leo file path
-input_path = '../../data/Acute_Inflammations/new_data.tsv'
-main_path = "./kMeans/src/main.leo"
+input_path = 'data/Acute_Inflammations/new_data.tsv'
+main_path = "src/k_means/kMeans/src/main.leo"
 
 # Normal test case from k_means.py
 # dataset = [2, 6]  # New point to be predicted
